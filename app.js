7108042bcc991114e44316d535359306fe9f2a9c
pjd <pjdonch@gmail.com>
2020-04-05 02:17:55 -0400

    Restyled

---
+++ app.js
@@ -0,0 +1,28 @@
+const entry = document.getElementById("entry")
+const list = document.getElementById("list")
+
+document.getElementById("gen-links").addEventListener(
+  'click',
+  () => {
+    // remove all elements from links
+    while (list.firstChild) list.removeChild(list.firstChild);
+
+    // get link items from text entry
+    const links = entry.value.trim().split('\n');
+
+    for (const link of links) {
+
+      const filename = link.split('\\').pop().split('/').pop();
+
+      const row = document.createElement('tr');
+      row.innerHTML = `<td><a href="${link}">${filename}</a></td>`
+
+      list.appendChild(row);
+
+    }
+
+
+
+
+  }
+);
